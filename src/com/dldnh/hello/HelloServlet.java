91fa07082ecbfefef1e23e4c5496d1ad5e2ebd13
Dave Diamond <dave.diamond@oracle.com>
2018-12-13 14:40:08 -0500

    Initial commit

---
+++ src/com/dldnh/hello/HelloServlet.java
@@ -0,0 +1,66 @@
+package com.dldnh.hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+//    private String ip;
+
+    public HelloServlet() {
+//        try (final DatagramSocket socket = new DatagramSocket()) {
+//            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+//            ip = socket.getLocalAddress().getHostAddress();
+//        } catch (SocketException | UnknownHostException e) {
+//            ip = e.getLocalizedMessage();
+//            e.printStackTrace();
+//        }
+    }
+
+    private String getAddress() {
+        StringBuilder str = new StringBuilder();
+        try {
+            Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
+            while (ifaces.hasMoreElements()) {
+                NetworkInterface iface = ifaces.nextElement();
+                if (iface.isLoopback()) {
+                    continue;
+                }
+                Enumeration<InetAddress> addrs = iface.getInetAddresses();
+                while (addrs.hasMoreElements()) {
+                    InetAddress addr = addrs.nextElement();
+                    byte[] bytes = addr.getAddress();
+                    if (bytes.length != 4) {
+                        continue;
+                    }
+                    if (str.length() > 0) {
+                        str.append(", ");
+                    }
+                    for (int i = 0; i < 4; i++) {
+                        int n = bytes[i] & 0xff;
+                        str.append(n);
+                        if (i < 3) {
+                            str.append(".");
+                        }
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+            return e.getLocalizedMessage();
+        }
+        return str.toString();
+    }
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws IOException {
+        final PrintWriter w = response.getWriter();
+        w.println("Hello, world! " + getAddress());
+    }
+}
